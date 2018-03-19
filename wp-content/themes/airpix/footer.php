045092081e47587e8f8003ff6eaa242389db05e1
lthanhbinh <lthanhbinh@tma.com.vn>
2018-03-19 22:25:15 +0700

    abc

---
+++ wp-content/themes/airpix/footer.php
@@ -0,0 +1,43 @@
+<!-- footer -->
+<div class="footer">
+    <div class="container">
+        <div class="footer-top">
+            <div class="footer-top-nav">
+                <ul>
+                    <li><a href="about.html">About</a></li>
+                    <li><a href="press.html">Press</a></li>
+                    <li><a href="copyright.html">Copyright</a></li>
+                    <li><a href="creators.html">Creators</a></li>
+                    <li><a href="#">Advertise</a></li>
+                    <li><a href="developers.html">Developers</a></li>
+                </ul>
+            </div>
+            <div class="footer-bottom-nav">
+                <ul>
+                    <li><a href="terms.html">Terms</a></li>
+                    <li><a href="privacy.html">Privacy</a></li>
+                    <li><a href="#small-dialog4" class="play-icon popup-with-zoom-anim">Send feedback</a></li>
+                    <li><a href="privacy.html">Policy & Safety </a></li>
+                    <li><a href="try.html">Try something new!</a></li>
+                    <li><p>Copyright © 2015 My Play. All Rights Reserved. | Design by <a href="http://w3layouts.com/">W3layouts</a></p></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- //footer -->
+<div class="clearfix"> </div>
+<div class="drop-menu">
+<ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu4">
+<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Regular link</a></li>
+<li role="presentation" class="disabled"><a role="menuitem" tabindex="-1" href="#">Disabled link</a></li>
+<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Another link</a></li>
+</ul>
+</div>
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="js/bootstrap.min.js"></script>
+<!-- Just to make our placeholder images work. Don't actually copy the next line! -->
+</body>
+</html>
---
+++ wp-content/themes/airpix/functions.php
@@ -0,0 +1,3 @@
+<?php
+add_filter('deprecated_constructor_trigger_error', '__return_false');
+ ?>
