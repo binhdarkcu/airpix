814b7681726968d0af9b52abd769d4cebdcbc846
lthanhbinh <lthanhbinh@tma.com.vn>
2018-03-23 00:52:01 +0700

    For Tung

---
+++ wp-content/themes/airpix-video/index.php
@@ -0,0 +1,162 @@
+<?php get_header()?>
+		<!-- upload -->
+        <div class="col-sm-3 col-md-2 sidebar">
+			<div class="top-navigation">
+				<div class="t-menu">MENU</div>
+				<div class="t-img">
+					<img src="images/lines.png" alt="">
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+				<div class="drop-navigation drop-navigation">
+				  <ul class="nav nav-sidebar">
+					<li class="active"><a href="index.html" class="home-icon"><span class="glyphicon glyphicon-home" aria-hidden="true"></span>Home</a></li>
+					<li><a href="shows.html" class="user-icon"><span class="glyphicon glyphicon-home glyphicon-blackboard" aria-hidden="true"></span>TV Shows</a></li>
+					<li><a href="history.html" class="sub-icon"><span class="glyphicon glyphicon-home glyphicon-hourglass" aria-hidden="true"></span>History</a></li>
+
+					<li><a href="news.html" class="news-icon"><span class="glyphicon glyphicon-envelope" aria-hidden="true"></span>News</a></li>
+				  </ul>
+				  <!-- script-for-menu -->
+						<script>
+							$( ".top-navigation" ).click(function() {
+							$( ".drop-navigation" ).slideToggle( 300, function() {
+							// Animation complete.
+							});
+							});
+						</script>
+					<div class="side-bottom">
+						<div class="side-bottom-icons">
+							<ul class="nav2">
+								<li><a href="#" class="facebook"> </a></li>
+								<li><a href="#" class="facebook twitter"> </a></li>
+								<li><a href="#" class="facebook chrome"> </a></li>
+								<li><a href="#" class="facebook dribbble"> </a></li>
+							</ul>
+						</div>
+						<div class="copyright">
+							<p>Copyright © 2015 My Play. All Rights Reserved | Design by <a href="http://w3layouts.com/">W3layouts</a></p>
+						</div>
+					</div>
+				</div>
+        </div>
+        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+            <div class="main-grids">
+                <div class="top-grids">
+					<div class="recommended-info">
+						<h3>Recent Videos</h3>
+					</div>
+                    <div class="col-md-4 resent-grid recommended-grid slider-top-grids">
+						<div class="resent-grid-img recommended-grid-img">
+							<a href="single.html"><img src="images/t1.jpg" alt=""></a>
+							<div class="time">
+								<p>3:04</p>
+							</div>
+							<div class="clck">
+								<span class="glyphicon glyphicon-time" aria-hidden="true"></span>
+							</div>
+						</div>
+						<div class="resent-grid-info recommended-grid-info">
+							<h3><a href="single.html" class="title title-info">Pellentesque vitae pulvinar tortor nullam interdum metus a imperdiet</a></h3>
+							<ul>
+								<li><p class="author author-info"><a href="#" class="author">John Maniya</a></p></li>
+								<li class="right-list"><p class="views views-info">2,114,200 views</p></li>
+							</ul>
+						</div>
+					</div>
+                    <div class="col-md-4 resent-grid recommended-grid slider-top-grids">
+						<div class="resent-grid-img recommended-grid-img">
+							<a href="single.html"><img src="images/t1.jpg" alt=""></a>
+							<div class="time">
+								<p>3:04</p>
+							</div>
+							<div class="clck">
+								<span class="glyphicon glyphicon-time" aria-hidden="true"></span>
+							</div>
+						</div>
+						<div class="resent-grid-info recommended-grid-info">
+							<h3><a href="single.html" class="title title-info">Pellentesque vitae pulvinar tortor nullam interdum metus a imperdiet</a></h3>
+							<ul>
+								<li><p class="author author-info"><a href="#" class="author">John Maniya</a></p></li>
+								<li class="right-list"><p class="views views-info">2,114,200 views</p></li>
+							</ul>
+						</div>
+					</div>
+                    <div class="col-md-4 resent-grid recommended-grid slider-top-grids">
+						<div class="resent-grid-img recommended-grid-img">
+							<a href="single.html"><img src="images/t1.jpg" alt=""></a>
+							<div class="time">
+								<p>3:04</p>
+							</div>
+							<div class="clck">
+								<span class="glyphicon glyphicon-time" aria-hidden="true"></span>
+							</div>
+						</div>
+						<div class="resent-grid-info recommended-grid-info">
+							<h3><a href="single.html" class="title title-info">Pellentesque vitae pulvinar tortor nullam interdum metus a imperdiet</a></h3>
+							<ul>
+								<li><p class="author author-info"><a href="#" class="author">John Maniya</a></p></li>
+								<li class="right-list"><p class="views views-info">2,114,200 views</p></li>
+							</ul>
+						</div>
+					</div>
+                    <div class="col-md-4 resent-grid recommended-grid slider-top-grids">
+						<div class="resent-grid-img recommended-grid-img">
+							<a href="single.html"><img src="images/t1.jpg" alt=""></a>
+							<div class="time">
+								<p>3:04</p>
+							</div>
+							<div class="clck">
+								<span class="glyphicon glyphicon-time" aria-hidden="true"></span>
+							</div>
+						</div>
+						<div class="resent-grid-info recommended-grid-info">
+							<h3><a href="single.html" class="title title-info">Pellentesque vitae pulvinar tortor nullam interdum metus a imperdiet</a></h3>
+							<ul>
+								<li><p class="author author-info"><a href="#" class="author">John Maniya</a></p></li>
+								<li class="right-list"><p class="views views-info">2,114,200 views</p></li>
+							</ul>
+						</div>
+					</div>
+                    <div class="col-md-4 resent-grid recommended-grid slider-top-grids">
+						<div class="resent-grid-img recommended-grid-img">
+							<a href="single.html"><img src="images/t1.jpg" alt=""></a>
+							<div class="time">
+								<p>3:04</p>
+							</div>
+							<div class="clck">
+								<span class="glyphicon glyphicon-time" aria-hidden="true"></span>
+							</div>
+						</div>
+						<div class="resent-grid-info recommended-grid-info">
+							<h3><a href="single.html" class="title title-info">Pellentesque vitae pulvinar tortor nullam interdum metus a imperdiet</a></h3>
+							<ul>
+								<li><p class="author author-info"><a href="#" class="author">John Maniya</a></p></li>
+								<li class="right-list"><p class="views views-info">2,114,200 views</p></li>
+							</ul>
+						</div>
+					</div>
+                    <div class="col-md-4 resent-grid recommended-grid slider-top-grids">
+						<div class="resent-grid-img recommended-grid-img">
+							<a href="single.html"><img src="images/t1.jpg" alt=""></a>
+							<div class="time">
+								<p>3:04</p>
+							</div>
+							<div class="clck">
+								<span class="glyphicon glyphicon-time" aria-hidden="true"></span>
+							</div>
+						</div>
+						<div class="resent-grid-info recommended-grid-info">
+							<h3><a href="single.html" class="title title-info">Pellentesque vitae pulvinar tortor nullam interdum metus a imperdiet</a></h3>
+							<ul>
+								<li><p class="author author-info"><a href="#" class="author">John Maniya</a></p></li>
+								<li class="right-list"><p class="views views-info">2,114,200 views</p></li>
+							</ul>
+						</div>
+					</div>
+                </div>
+            </div>
+        </div>
+<div class="clearfix"> </div>
+<?php get_footer()?>
+
+<!---728x90--->
