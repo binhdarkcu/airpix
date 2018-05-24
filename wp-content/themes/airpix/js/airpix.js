e1d85cd15ca9c393c183e0f7b8129c3eac4596fe
Thanh Che <cqthanh@tma.com.vn>
2018-05-24 09:39:32 +0700

    Hardcoded locations

--- wp-content/themes/airpix/js/airpix.js
+++ wp-content/themes/airpix/js/airpix.js
@@ -1,4 +1,23 @@
 var map;
+//example locations
+var lab6TMA = {lat: 10.85606544054808, lng: 106.63119583072796}
+var pilotMarkers = [];
+var pilots = [
+    {lat: 10.855838898689916, lng: 106.63088737669125},
+    {lat: 10.855294686885607, lng: 106.63118778410092},
+    {lat: 10.85558129010037, lng: 106.62981878651135},
+    {lat: 10.855404797916394, lng: 106.62985901964657},
+    {lat: 10.856220085566322, lng: 106.63203965557568},
+    {lat: 10.855301950621058, lng: 106.63112345845013},
+    {lat: 10.854595980599653, lng: 106.62988964230328},
+    {lat: 10.854050696642918, lng: 106.62893209368497},
+    {lat: 10.856934066000505, lng: 106.62997831691939},
+    {lat: 10.857287048457364, lng: 106.63182904113967},
+    {lat: 10.856581083126574, lng: 106.63104047168929},
+    {lat: 10.855290984991285, lng: 106.62810287249863},
+    {lat: 10.852909646514199, lng: 106.62956199420273},
+    {lat: 10.857511747837211, lng: 106.6311485200282}
+];
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: -34.397, lng: 150.644},
@@ -10,7 +29,7 @@ function initMap() {
     //get location on click
     google.maps.event.addListener(map, 'click', function (event) {
         console.log("lat:", event.latLng.lat());
-        console.log("lung:", event.latLng.lng());
+        console.log("long:", event.latLng.lng());
     });
 }
 
@@ -26,12 +45,25 @@ function getLocation() {
 function showCurrentPosition(position) {
     var lat = position.coords.latitude;
     var lng = position.coords.longitude;
-    map.setCenter(new google.maps.LatLng(lat, lng));
-    var marker = new google.maps.Marker({
-        position: {lat: lat, lng: lng},
-        draggable: true,
-        map: map,
-        title: 'Hello World!'
+    map.setCenter(new google.maps.LatLng(lab6TMA.lat, lab6TMA.lng));
+    pilotMarkers.push(
+            new google.maps.Marker({
+                position: lab6TMA,
+                draggable: true,
+                map: map,
+                title: 'You are here!'
+            })
+            );
+
+    pilots.forEach(function (item, index) {
+//        console.log("item: ", item);
+        pilotMarkers.push(
+                new google.maps.Marker({
+                    position: item,
+                    draggable: true,
+                    map: map,
+                    title: 'Pilot ' + index
+                }));
     });
 }
 jQuery(document).ready(function () {
